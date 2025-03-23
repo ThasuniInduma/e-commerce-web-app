ba709a50e43c25033844732c651f652d822e3871
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-03-23 17:19:22 +0530

    nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import '.Navbar.css'
+import './Navbar.css'
+import logo from '../Assetes/logo.png'
+import cart_icon from '../Assetes/cart_icon.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>Shopper</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className='nav-login'>
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
     </div>
   )
 }
