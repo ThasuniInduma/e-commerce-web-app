3e2bed5aaa99c625a5d616d4f5bc3808ce11e1e4
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-03-28 22:49:47 +0530

    footer css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Footer.css'
 import footer_logo from '../Assetes/logo_big.png'
+import instagram_icon from '../Assetes/instagram_icon.png'
+import printerest_icon from '../Assetes/pintester_icon.png'
+import whatsapp_icon from '../Assetes/whatsapp_icon.png'
 
 const Footer = () => {
   return (
@@ -18,8 +21,18 @@ const Footer = () => {
       </ul>
       <div className="footer-social-icon">
         <div className="footer-icons-container">
-            <img src={} alt="" />
+            <img src={instagram_icon} alt="" />
         </div>
+        <div className="footer-icons-container">
+            <img src={printerest_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={whatsapp_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2025 - All Right Reserved</p>
       </div>
     </div>
   )
