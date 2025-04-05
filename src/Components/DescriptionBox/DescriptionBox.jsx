7b575a66ac89bfaa9c555b4e6fed678bbf288cce
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-04-05 21:46:36 +0530

    description boxes added

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div classname='descriptionbox'>
+      <div className="descriptionbox-nav">
+        <div className="descriptionbox-nav-box">
+            Discription
+        </div>
+        <div className="descriptionbox-nav-box fade">
+            Reviews (122)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Turn heads in the Midnight Bloom Maxi – a flowy, full-length dress crafted from luxe chiffon and kissed with floral detailing. Perfect for evening galas or romantic dinners, this dress features a flattering wrap silhouette and adjustable waist tie. Elegance, redefined.</p>
+        <p>Make a statement in comfort with our Core Street Graphic Tee. Made from 100% breathable cotton, this shirt pairs bold graphics with a relaxed fit. Ideal for layering or standing out on its own streetwear never felt this good.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
